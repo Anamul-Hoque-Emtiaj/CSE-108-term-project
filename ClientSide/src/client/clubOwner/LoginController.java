f1bd49ab5d67f06c9e2da272ee0afe34ebc4ebca
A H Emtiaz <1905113@ugrad.cse.buet.ac.bd>
2021-07-16 09:58:48 +0600

    Added Club Menu

---
+++ ClientSide/src/client/clubOwner/LoginController.java
@@ -0,0 +1,11 @@
+package client.clubOwner;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    public void login(ActionEvent event) {
+    }
+
+    public void cancel(ActionEvent event) {
+    }
+}
---
+++ ClientSide/src/client/clubOwner/MenuController.java
@@ -0,0 +1,29 @@
+package client.clubOwner;
+
+import javafx.event.ActionEvent;
+
+public class MenuController {
+    public void clubDetails(ActionEvent event) {
+    }
+
+    public void searchPlayer(ActionEvent event) {
+    }
+
+    public void addPlayer(ActionEvent event) {
+    }
+
+    public void buyPlayer(ActionEvent event) {
+    }
+
+    public void changePassword(ActionEvent event) {
+    }
+
+    public void exit(ActionEvent event) {
+    }
+
+    public void sellRequest(ActionEvent event) {
+    }
+
+    public void pendingRequest(ActionEvent event) {
+    }
+}
